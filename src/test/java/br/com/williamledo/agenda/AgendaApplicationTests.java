99b08815defd65490506db6c50711851d9ec533a
William Lêdo <williamledoc@gmail.com>
2023-12-04 01:10:37 -0300

    commit inicial do projeto

---
+++ src/test/java/br/com/williamledo/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.williamledo.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
